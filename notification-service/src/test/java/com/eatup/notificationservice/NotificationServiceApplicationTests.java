392d642be7be95074759b8e8a562ae6de75e3655
Onur Teber <onur.teber@softtech.com.tr>
2020-12-13 15:12:05 +0300

    notification-service eklendi

---
+++ notification-service/src/test/java/com/eatup/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatup.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
